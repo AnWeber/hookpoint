12e153057804c8b3a3bc9e2fb4e2f7728a6d6025
Andreas Weber <weber.andreas@gmail.com>
2021-12-25 21:15:36 +0100

    fix: error in waterFallhook getNextArgs fixed

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,12 @@
 
 - removed not needed files from publish release
 
+## 1.0.3 (2021-12-25)
+
+#### Fix
+
+- getNextArgs replaces not result
+
 ## 1.0.0 (2021-12-23)
 
 #### Feature
--- src/hooks/waterfallHook.ts
+++ src/hooks/waterfallHook.ts
@@ -1,21 +1,22 @@
 import { Hook } from './hook';
 
 export class WaterfallHook<TArgs extends unknown[], TBail = TArgs[0]> extends Hook<
-TArgs,
-TArgs[0] | TBail,
-TArgs[0] | TBail
+  TArgs,
+  TArgs[0] | TBail,
+  TArgs[0] | TBail
 > {
-constructor(bailOut?: ((arg: TArgs[0] | TBail) => boolean) | undefined) {
-  super(bailOut);
-}
-protected getNextArgs(next: TArgs[0], args: TArgs): TArgs {
-  return args.splice(0, 1, next) as TArgs;
-}
+  constructor(bailOut?: ((arg: TArgs[0] | TBail) => boolean) | undefined) {
+    super(bailOut);
+  }
+  protected getNextArgs(next: TArgs[0], args: TArgs): TArgs {
+    args[0] = next;
+    return args;
+  }
 
-protected getMergedResults(results: TArgs[0][], args: TArgs): TArgs[0] {
-  return results.pop() || args[0];
-}
-protected initNew() {
-  return new WaterfallHook<TArgs, TBail>(this.bailOut);
-}
+  protected getMergedResults(results: TArgs[0][], args: TArgs): TArgs[0] {
+    return results.pop() || args[0];
+  }
+  protected initNew() {
+    return new WaterfallHook<TArgs, TBail>(this.bailOut);
+  }
 }
